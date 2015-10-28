0e1042bc88edcc946f3ec5d469a21ef7171d880a
Jongwon(Jerry) Han <addnull@gmail.com>
2015-10-29 04:28:36 +0900

    .

--- wildcard_subdomains_with_dnsmasq/README.md
+++ wildcard_subdomains_with_dnsmasq/README.md
@@ -3,4 +3,4 @@ How to Play
 
 1. create a VM: 'vagrant up'
 2. connect ssh: 'vagrant ssh'
-3. ping test (in VM): 'ping aaaa.mydomain.com' or 'ping bb.mydomain.com'
+3. ping test (in VM): 'ping aaaa.dummy_domain.com' or 'ping bb.dummy_domain.com'
--- wildcard_subdomains_with_dnsmasq/provisioning.sh
+++ wildcard_subdomains_with_dnsmasq/provisioning.sh
@@ -4,6 +4,6 @@ set -e
 apt-get -y install dnsmasq
 
 cp /etc/dnsmasq.conf /etc/dnsmasq.conf.bak
-echo 'address=/.mydomain.com/127.0.0.1' >> /etc/dnsmasq.conf
+echo 'address=/.dummy_domain.com/127.0.0.1' >> /etc/dnsmasq.conf
 
 service dnsmasq restart
